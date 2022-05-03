0cdf4e581d21c2e9c121387a13fd14d4cd9bb102
Carlos <carlos@carloslandia.com>
2022-05-03 09:04:55 +0200

    Ejercicio Test 2

---
+++ src/main/kotlin/ObtenerPokemonsRequest.kt
@@ -0,0 +1,38 @@
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+class ObtenerPokemonsRequest {
+
+    class ExceptionEnRed : Exception()
+
+    class ExceptionEnId : Exception()
+
+    companion object {
+        fun get(pokemonPrimeroId : Int, pokemonUltimoId : Int): Array<Pokemon> {
+            if (pokemonPrimeroId > pokemonUltimoId)
+                throw ExceptionEnId()
+
+            val client = OkHttpClient()
+            val list = mutableListOf<Pokemon>()
+            for (i in pokemonPrimeroId .. pokemonUltimoId) {
+                val request = Request.Builder()
+                    .url("https://pokeapi.co/api/v2/pokemon/${i}")
+                    .build()
+                val response = client.newCall(request).execute()
+
+                if (response.isSuccessful) {
+                    response.body?.string().let { responseBody ->
+
+                        list.add(gson.fromJson(responseBody, Pokemon::class.java))
+                    }
+
+                } else
+                    throw ExceptionEnRed()
+            }
+            list.add(Pokemon(0, 0, "Unknown", 0))
+            return list.toTypedArray()
+        }
+
+    }
+
+}
---
+++ src/test/java/ObtenerPokemonsTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ObtenerPokemonsTest {
+
+
+
+    @Test
+    public void probarPokemonExceptionRed(){
+        // TODO que se lanza la exception correcta
+
+    }
+
+    @Test
+    public void probarPokemonExceptionID(){
+        // TODO que se lanza la exception correcta
+
+    }
+
+    @Test
+    public void probarPokemon1A10Devuelve10Pokemon(){
+        // TODO Realiza lo mismo que el ejercicio anterior pero para un rango de Pokémons que van desde el 140 al 150.
+        //  Estos números deberían poder cambiar fácilmente
+        int minId = 1;
+        int maxId = 10;
+
+    }
+
+    @Test
+    public void probarPokemon140A150NombreEId(){
+        // TODO Realiza lo mismo que el ejercicio anterior pero para un rango de Pokémons que van desde el 140 al 150.
+        //  Estos números deberían poder cambiar fácilmente
+        int minId = 140;
+        int maxId = 150;
+
+    }
+}
