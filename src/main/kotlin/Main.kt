1f84108bc9cbe187c2beab51b85e336274528aba
cbellmont <carlos.bellmont@gmail.com>
2022-05-04 09:22:25 +0200

    Ejercicio Entregable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,24 @@ import com.google.gson.Gson
 
 var gson = Gson()
 
+class PokemonInvalido : Exception()
+
 fun main() {
+    val pokemon1 = ObtenerPokemonRequest.get(1)
+    val pokemon2 = ObtenerPokemonRequest.get(1)
+
+}
 
 
+/**
+ *  Esta función utiliza las varialbes pokemon1 y pokemon2 para calcular que ponemos gana en una pelea
+ *  @return al pokemon ganador
+ */
+fun pelea(pokemon1: Pokemon?, pokemon2: Pokemon?): Pokemon? {
+    if (pokemon2 == null || pokemon1 == null) throw PokemonInvalido()
+    return when {
+        pokemon1.weight > pokemon2.weight -> pokemon1
+        pokemon1.weight < pokemon2.weight -> pokemon2
+        else -> null
+    }
 }
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,9 @@
+public class MainTest {
+
+    // TODO realiza los test que consideres necesarios para comprobar que la función pelea() ubicada en main funciona
+    //  correctamente. El comportamiento esperado es que el pokémon más gordo ganará, es decir, la función devuelve al
+    //  pokémon más gordo. En caso de empate (ambos pesan lo mismo) entonces la función devuelve null. En caso de que
+    //  alguno de los pokémon sea null, entonces devolverá una "exception" de la clase "PokemonInvalido"
+
+
+}
