3ec8d5ac09a78024d316711ea7d72f60ef7ceac4
cbellmont <carlos.bellmont@gmail.com>
2022-05-04 09:24:19 +0200

    Enunciado actualizado

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,9 +1,10 @@
 public class MainTest {
 
-    // TODO realiza los test que consideres necesarios para comprobar que la función pelea() ubicada en main funciona
-    //  correctamente. El comportamiento esperado es que el pokémon más gordo ganará, es decir, la función devuelve al
-    //  pokémon más gordo. En caso de empate (ambos pesan lo mismo) entonces la función devuelve null. En caso de que
-    //  alguno de los pokémon sea null, entonces devolverá una "exception" de la clase "PokemonInvalido"
+    // TODO realiza los test que consideres necesarios para comprobar que la función "pelea()" a la que le entran 2
+    //  argumentos (pokemon1 y un pokemon2) ubicada en main funciona correctamente. El comportamiento esperado es que
+    //  el pokémon más gordo ganará, es decir, la función devuelve al pokémon más gordo. En caso de empate (ambos pesan
+    //  lo mismo) entonces la función devuelve null. En caso de que alguno de los pokémon sea null, entonces devolverá
+    //  una "exception" de la clase "PokemonInvalido"
 
 
 }
